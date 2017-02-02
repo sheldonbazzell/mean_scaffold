976ef6114a506eb184a24b74ae0fa433bb5ff3b2
Thomas Fowler <tkfowler13@gmail.com>
2017-02-01 16:43:28 -0800

    removed test folders

--- Demo/server/models/user.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-var Schema   = mongoose.Schema,
-    userSchema = new Schema({
-    	name: {type: String},
- },{timestamps:true});
- 	  mongoose.model('USer', userSchema)
